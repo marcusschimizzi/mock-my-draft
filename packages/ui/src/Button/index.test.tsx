16579d6235349e8256fd243cb61c61cf391f8288
schimizzimj <marcusjschimizzi@gmail.com>
2024-04-04 10:00:34 -0700

    update button tests

--- packages/ui/src/Button/index.test.tsx
+++ packages/ui/src/Button/index.test.tsx
@@ -1,11 +1,18 @@
-import { createRoot } from "react-dom/client";
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import "@testing-library/jest-dom";
 import { Button } from ".";
 
 describe("Button", () => {
   it("renders without crashing", () => {
-    const div = document.createElement("div");
-    const root = createRoot(div);
-    root.render(<Button>Hello</Button>);
-    root.unmount();
+    render(<Button>Test</Button>);
+  });
+
+  it("fires the onClick event when button is pressed", async () => {
+    const onClick = jest.fn();
+    render(<Button onClick={onClick}>Test</Button>);
+
+    await userEvent.click(screen.getByText("Test"));
+    expect(onClick).toHaveBeenCalled();
   });
 });
