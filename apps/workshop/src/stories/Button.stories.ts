aa0d38c3ab0dd9316f4e7b1b22096f68d0a81ed9
schimizzimj <marcusjschimizzi@gmail.com>
2024-04-05 01:13:21 -0700

    add button type styles

--- apps/workshop/src/stories/Button.stories.ts
+++ apps/workshop/src/stories/Button.stories.ts
@@ -1,11 +1,11 @@
 import type { Meta, StoryObj } from "@storybook/react";
 import { fn } from "@storybook/test";
-import { CounterButton } from "@mock-my-draft/ui";
+import { Button } from "@mock-my-draft/ui";
 
 // More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
 const meta = {
   title: "Example/Button",
-  component: CounterButton,
+  component: Button,
   parameters: {
     // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
     layout: "centered",
@@ -13,12 +13,10 @@ const meta = {
   // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
   tags: ["autodocs"],
   // More on argTypes: https://storybook.js.org/docs/api/argtypes
-  argTypes: {
-    backgroundColor: { control: "color" },
-  },
+  argTypes: {},
   // Use `fn` to spy on the onClick arg, which will appear in the actions panel once invoked: https://storybook.js.org/docs/essentials/actions#action-args
   args: { onClick: fn() },
-} satisfies Meta<typeof CounterButton>;
+} satisfies Meta<typeof Button>;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
@@ -26,27 +24,35 @@ type Story = StoryObj<typeof meta>;
 // More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
 export const Primary: Story = {
   args: {
-    primary: true,
-    label: "Button",
+    kind: "primary",
+    children: "Button",
   },
 };
 
 export const Secondary: Story = {
   args: {
-    label: "Button",
+    kind: "secondary",
+    children: "Button",
   },
 };
 
-export const Large: Story = {
+export const Danger: Story = {
   args: {
-    size: "large",
-    label: "Button",
+    kind: "danger",
+    children: "Button",
   },
 };
 
-export const Small: Story = {
+export const Outline: Story = {
   args: {
-    size: "small",
-    label: "Button",
+    kind: "outline",
+    children: "Button",
+  },
+};
+
+export const Link: Story = {
+  args: {
+    kind: "link",
+    children: "Button",
   },
 };
--- packages/ui/src/index.tsx
+++ packages/ui/src/index.tsx
@@ -1,2 +1,4 @@
 export { Link } from "./Link";
 export { CounterButton } from "./CounterButton";
+export { Button } from "./Button";
+export { Slider } from "./Slider";
