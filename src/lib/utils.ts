851d0ac802bdf65131bd271339bd85cf757ee823
schimizzimj <marcusjschimizzi@gmail.com>
2024-06-19 15:13:16 -0700

    add capitalize util

---
+++ src/lib/utils.ts
@@ -0,0 +1,6 @@
+export function capitalize(input: string): string {
+  const words = input.split(" ");
+  return words
+    .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+    .join(" ");
+}
