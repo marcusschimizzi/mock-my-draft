c710984b7e85d6ac2e30837477eb4205b4a27a07
schimizzimj <marcusjschimizzi@gmail.com>
2024-06-19 12:23:14 -0700

    add simple display table

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,66 @@
+import {
+  Container,
+  Heading,
+  Table,
+  TableContainer,
+  Tbody,
+  Td,
+  Th,
+  Thead,
+  Tr,
+} from "@chakra-ui/react";
 import React from "react";
 
-export default function Home() {
+async function getData() {
+  const res = await fetch("http://localhost:3000/api");
+
+  if (!res.ok) {
+    throw new Error("Failed to fetch data");
+  }
+
+  return res.json();
+}
+
+export default async function Home() {
+  const data = await getData();
+  const keys = Object.keys(data[0]);
+  const teamIndex = keys.findIndex((value) => value === "team");
+  const averageIndex = keys.findIndex((value) => value === "average");
   return (
-    <main>
-      <h1>Mock my draft</h1>
-    </main>
+    <Container as="main" maxW="container.xl">
+      <Heading>2024 NFL Draft Class Grades</Heading>
+      <TableContainer>
+        <Table variant="simple">
+          <Thead>
+            <Tr>
+              <Th>Team</Th>
+              {keys
+                .filter((key) => key !== "team")
+                .map((key) => (
+                  <Th key={key}>{key}</Th>
+                ))}
+            </Tr>
+          </Thead>
+          <Tbody>
+            {data.map((entry) => (
+              <Tr key={entry.team}>
+                <Td>{entry.team}</Td>
+                {Object.values(entry).map((value: any, index: number) => {
+                  if (index === teamIndex) {
+                    return null;
+                  }
+                  if (index === averageIndex) {
+                    return (
+                      <Td key={keys[index]}>{Number(value).toFixed(2)}</Td>
+                    );
+                  }
+                  return <Td key={keys[index]}>{value}</Td>;
+                })}
+              </Tr>
+            ))}
+          </Tbody>
+        </Table>
+      </TableContainer>
+    </Container>
   );
 }
