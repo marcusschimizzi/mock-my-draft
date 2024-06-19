eb16dbadfb9431f28741fb33f33cac8613421edd
schimizzimj <marcusjschimizzi@gmail.com>
2024-06-18 22:44:05 -0700

    create simple text logo

---
+++ src/components/logo.tsx
@@ -0,0 +1,12 @@
+import { Box, Text } from "@chakra-ui/react";
+import React from "react";
+
+export default function Logo(props) {
+  return (
+    <Box {...props}>
+      <Text fontSize="large" fontWeight="bold">
+        Mock My Draft
+      </Text>
+    </Box>
+  );
+}
