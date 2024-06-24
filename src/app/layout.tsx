72c0576986e4c5d28acfba96e6da054d3c3c520c
schimizzimj <marcusjschimizzi@gmail.com>
2024-06-23 20:39:21 -0700

    fix mobile scaling issue

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,10 +4,18 @@ import "./globals.css";
 import { PublicLayout } from "@/layouts/public-layout";
 import { ColorModeScript } from "@chakra-ui/react";
 import { theme } from "@/config/theme";
+import { Viewport } from "next";
 
 export const metadata = {
   title: "Mock My Draft",
   description: "Check out how previous NFL draft classes were graded",
+};
+
+export const viewport: Viewport = {
+  initialScale: 1,
+  maximumScale: 1,
+  width: "device-width",
+  themeColor: "#000000",
 };
 
 export default function RootLayout({ children }: { children: ReactNode }) {
