d459f9fef055854e53e677c5b29fd9d5435a06b7
schimizzimj <marcusjschimizzi@gmail.com>
2024-07-11 14:12:24 -0700

    setup project with nx from scratch

---
+++ libs/visualizations/README.md
@@ -0,0 +1,7 @@
+# visualizations
+
+This library was generated with [Nx](https://nx.dev).
+
+## Running unit tests
+
+Run `nx test visualizations` to execute the unit tests via [Vitest](https://vitest.dev/).
--- libs/visualizations/jest.config.ts
+++ libs/visualizations/jest.config.ts
@@ -1,11 +1,11 @@
 /* eslint-disable */
 export default {
-  displayName: 'admin',
+  displayName: 'visualizations',
   preset: '../../jest.preset.js',
   transform: {
     '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nx/react/plugins/jest',
-    '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nx/next/babel'] }],
+    '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nx/react/babel'] }],
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
-  coverageDirectory: '../../coverage/apps/admin',
+  coverageDirectory: '../../coverage/libs/visualizations',
 };
