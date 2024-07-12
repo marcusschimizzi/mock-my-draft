68e7ee3744dfefc0fc081edef8933c4e475accf7
schimizzimj <marcusjschimizzi@gmail.com>
2024-07-11 23:42:27 -0700

    add additional types

--- apps/admin/src/types/index.ts
+++ apps/admin/src/types/index.ts
@@ -13,3 +13,47 @@ export type LoginData = {
   username: string;
   password: string;
 };
+
+export enum Conference {
+  NFC = 'nfc',
+  AFC = 'afc',
+}
+
+export enum Division {
+  North = 'north',
+  South = 'south',
+  East = 'east',
+  West = 'west',
+}
+
+export type Team = Entity & {
+  name: string;
+  location: string;
+  nickname: string;
+  conference: Conference;
+  division: Division;
+  abbreviation: string;
+  slug: string;
+  logo?: string;
+  colors?: string[];
+};
+
+export const defaultTeam: Partial<Team> = {
+  name: '',
+  location: '',
+  nickname: '',
+  abbreviation: '',
+  slug: '',
+};
+
+export type Source = Entity & {
+  name: string;
+  slug: string;
+  baseUrl: string;
+};
+
+export const defaultSource: Partial<Source> = {
+  name: '',
+  slug: '',
+  baseUrl: '',
+};
