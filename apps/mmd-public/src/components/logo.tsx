b70210e4689c1bb401f01c0d206cb53729b9923b
schimizzimj <marcusjschimizzi@gmail.com>
2024-07-15 11:31:51 -0700

    update e2e tests

--- apps/mmd-public/src/components/logo.tsx
+++ apps/mmd-public/src/components/logo.tsx
@@ -3,7 +3,7 @@ import { Box, ChakraProps, Text } from '@chakra-ui/react';
 export default function Logo(props: ChakraProps) {
   return (
     <Box {...props}>
-      <Text fontSize="lg" fontWeight="bold">
+      <Text fontSize="lg" fontWeight="bold" as="h1">
         Mock My Draft
       </Text>
     </Box>
