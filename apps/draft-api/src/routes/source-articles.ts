628e2d783939e0f5d51387f710599ee08f0b7e04
schimizzimj <marcusjschimizzi@gmail.com>
2024-07-16 10:02:58 -0700

    add query validation to source article route

--- apps/draft-api/src/routes/source-articles.ts
+++ apps/draft-api/src/routes/source-articles.ts
@@ -1,10 +1,16 @@
 import { Router } from 'express';
 import { SourceArticlesController } from '../controllers/source-articles-controller';
+import { validateQuery } from '../middleware/validate-query.middleware';
+import { SourceArticleQueryDto } from '../dtos/source-article.dto';
 
 const router = Router();
 const sourceArticlesController = new SourceArticlesController();
 
-router.get('/', sourceArticlesController.getAllSourceArticles);
+router.get(
+  '/',
+  validateQuery(SourceArticleQueryDto),
+  sourceArticlesController.getAllSourceArticles,
+);
 router.get('/:id', sourceArticlesController.getSourceArticleById);
 router.post('/', sourceArticlesController.createSourceArticle);
 router.put('/:id', sourceArticlesController.updateSourceArticle);
