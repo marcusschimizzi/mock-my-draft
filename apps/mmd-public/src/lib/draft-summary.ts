31865dc0c99d4f58f3e964e8680343471581211b
schimizzimj <marcusjschimizzi@gmail.com>
2024-07-18 02:15:44 -0700

    add draft summary api funcs

---
+++ apps/mmd-public/src/lib/draft-summary.ts
@@ -0,0 +1,40 @@
+import { DraftSummary, TeamDraftSummary } from '../types';
+import apiClient from './api-client';
+import { useQuery } from '@tanstack/react-query';
+
+export const getDraftSummary = async (year: number): Promise<DraftSummary> => {
+  return await apiClient.get(`/draft-summary/${year}`);
+};
+
+export const getTeamDraftSummary = async (
+  year: number,
+  teamId: string,
+): Promise<TeamDraftSummary> => {
+  return await apiClient.get(`/draft-summary/${year}/team/${teamId}`);
+};
+
+export const useDraftSummary = (year: number) => {
+  const { data, isFetching, isFetched } = useQuery({
+    queryKey: ['draft-summary', year],
+    queryFn: () => getDraftSummary(year),
+    initialData: null,
+  });
+
+  return {
+    draftSummary: data,
+    isLoading: isFetching && !isFetched,
+  };
+};
+
+export const useTeamDraftSummary = (year: number, teamId: string) => {
+  const { data, isFetching, isFetched } = useQuery({
+    queryKey: ['draft-summary', year, teamId],
+    queryFn: () => getTeamDraftSummary(year, teamId),
+    initialData: null,
+  });
+
+  return {
+    draftSummary: data,
+    isLoading: isFetching && !isFetched,
+  };
+};
