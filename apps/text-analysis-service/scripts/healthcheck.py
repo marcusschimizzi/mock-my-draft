4299515b4a317669e0dedd8d029c967a520f4b75
schimizzimj <marcusjschimizzi@gmail.com>
2024-07-20 00:46:53 -0700

    add healthcheck for text analysis service

---
+++ apps/text-analysis-service/scripts/healthcheck.py
@@ -0,0 +1,22 @@
+import sys
+import urllib.request
+import json
+
+def healthcheck():
+    try:
+        url = "http://localhost:8000/health"
+        with urllib.request.urlopen(url) as response:
+            data = json.loads(response.read().decode())
+            if response.getcode() == 200:
+                if data["status"] == "ok":
+                    print("Healthcheck passed")
+                    sys.exit(0)
+                else:
+                    print("Healthcheck failed")
+                    sys.exit(1)
+    except:
+        print("Healthcheck failed")
+        sys.exit(1)
+
+if __name__ == "__main__":
+    healthcheck()
