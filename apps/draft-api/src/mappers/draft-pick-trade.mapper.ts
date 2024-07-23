afe6495397fa5e352dca2f81739654ac690e5129
schimizzimj <marcusjschimizzi@gmail.com>
2024-07-22 23:20:49 -0700

    add draft pick trade mapper

---
+++ apps/draft-api/src/mappers/draft-pick-trade.mapper.ts
@@ -0,0 +1,57 @@
+import { DraftPick } from '../database/models/draft-pick';
+import { DraftPickTrade } from '../database/models/draft-pick-trade';
+import { Team } from '../database/models/team';
+import { CreateDraftPickTradeDto } from '../dtos/draft-pick-trade.dto';
+import { DraftPickResponseDto } from '../dtos/draft-pick.dto';
+
+export class DraftPickTradeMapper {
+  static toEntity(
+    dto: CreateDraftPickTradeDto,
+    draftPick: DraftPick,
+    fromTeam: Team,
+    toTeam: Team,
+  ): DraftPickTrade {
+    const entity = new DraftPickTrade();
+    entity.draftPick = draftPick;
+    entity.fromTeam = fromTeam;
+    entity.toTeam = toTeam;
+    entity.tradeDate = dto.tradeDate ? new Date(dto.tradeDate) : undefined;
+    entity.tradeDetails = dto.tradeDetails;
+    return entity;
+  }
+
+  static toUpdateEntity(
+    entity: DraftPickTrade,
+    dto: CreateDraftPickTradeDto,
+    draftPick?: DraftPick,
+    fromTeam?: Team,
+    toTeam?: Team,
+  ): DraftPickTrade {
+    if (dto.tradeDate !== undefined)
+      entity.tradeDate = dto.tradeDate ? new Date(dto.tradeDate) : undefined;
+    if (dto.tradeDetails !== undefined) entity.tradeDetails = dto.tradeDetails;
+    if (draftPick !== undefined) entity.draftPick = draftPick;
+    if (fromTeam !== undefined) entity.fromTeam = fromTeam;
+    if (toTeam !== undefined) entity.toTeam = toTeam;
+    return entity;
+  }
+
+  static toResponseDto(draftPickTrade: DraftPickTrade): DraftPickResponseDto {
+    return {
+      id: draftPickTrade.id,
+      round: draftPickTrade.draftPick.round,
+      pickNumber: draftPickTrade.draftPick.pickNumber,
+      year: draftPickTrade.draftPick.year,
+      originalTeam: {
+        id: draftPickTrade.fromTeam.id,
+        name: draftPickTrade.fromTeam.name,
+        abbreviation: draftPickTrade.fromTeam.abbreviation,
+      },
+      currentTeam: {
+        id: draftPickTrade.toTeam.id,
+        name: draftPickTrade.toTeam.name,
+        abbreviation: draftPickTrade.toTeam.abbreviation,
+      },
+    };
+  }
+}
