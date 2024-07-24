d886344adbcd3fb022b17ccdaf9950f058b2a1d0
schimizzimj <marcusjschimizzi@gmail.com>
2024-07-24 01:00:10 -0700

    fixing CI build issues

--- apps/admin/test/setup-tests.ts
+++ apps/admin/test/setup-tests.ts
@@ -2,3 +2,17 @@ jest.mock('next/navigation', () => ({
   usePathname: jest.fn(),
   useRouter: jest.fn().mockReturnValue({ push: jest.fn() }),
 }));
+
+Object.defineProperty(window, 'matchMedia', {
+  writable: true,
+  value: jest.fn().mockImplementation((query) => ({
+    matches: false,
+    media: query,
+    onchange: null,
+    addListener: jest.fn(), // Deprecated
+    removeListener: jest.fn(), // Deprecated
+    addEventListener: jest.fn(),
+    removeEventListener: jest.fn(),
+    dispatchEvent: jest.fn(),
+  })),
+});
