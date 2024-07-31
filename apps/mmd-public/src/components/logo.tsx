d8279935ce0fdb89c3667575b7ef327252922746
schimizzimj <marcusjschimizzi@gmail.com>
2024-07-31 13:43:32 -0700

    load logo explicitly rather than from public

--- apps/mmd-public/src/components/logo.tsx
+++ apps/mmd-public/src/components/logo.tsx
@@ -1,16 +1,12 @@
 import { Box, ChakraProps, HStack, Text } from '@chakra-ui/react';
 import Image from 'next/image';
+import logoSvg from '../images/logo.svg';
 
 export default function Logo(props: ChakraProps) {
   return (
     <Box {...props}>
       <HStack>
-        <Image
-          src="/logo.svg"
-          alt="Mock My Draft Logo"
-          width={50}
-          height={50}
-        />
+        <Image src={logoSvg} alt="Mock My Draft Logo" width={50} height={50} />
         <Text fontSize="lg" fontWeight="bold" as="h1">
           Mock My Draft
         </Text>
