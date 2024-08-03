08e03d58086d1946afd46a6ef227dd07e8bb4143
schimizzimj <marcusjschimizzi@gmail.com>
2024-08-02 22:02:47 -0700

    update express request size limits

--- apps/draft-api/src/main.ts
+++ apps/draft-api/src/main.ts
@@ -46,8 +46,8 @@ app.use(
   }),
 );
 app.use(cookieParser());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: '2mb' }));
+app.use(express.urlencoded({ extended: true, limit: '2mb' }));
 app.use(logger);
 
 app.use('/assets', express.static(path.join(__dirname, 'assets')));
