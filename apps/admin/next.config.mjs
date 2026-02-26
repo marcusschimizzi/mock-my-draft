f65ff520bafb948e440dc7e8c06b94173576b7c6
Marcus Schimizzi <marcusjschimizzi@gmail.com>
2026-02-25 23:02:27 -0600

    chore: remove withNx wrapper from Next.js configs

--- apps/admin/next.config.mjs
+++ apps/admin/next.config.mjs
@@ -1,10 +1,6 @@
-import { withNx, composePlugins } from '@nx/next';
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone',
 };
 
-const plugins = [withNx];
-
-export default composePlugins(...plugins)(nextConfig);
+export default nextConfig;
