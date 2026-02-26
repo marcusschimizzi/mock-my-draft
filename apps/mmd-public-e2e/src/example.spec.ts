f2b53403df3a610e5a3c4321827816817808b827
Marcus Schimizzi <marcusjschimizzi@gmail.com>
2026-02-26 08:54:34 -0600

    Fix lingering E2E failure from redesign

--- apps/mmd-public-e2e/src/example.spec.ts
+++ apps/mmd-public-e2e/src/example.spec.ts
@@ -6,9 +6,9 @@ test('has title', async ({ page }) => {
   // Expect there to be two h1 elements.
   expect(await page.locator('h1').count()).toBe(2);
 
-  // Both h1 elements should contain the text 'Mock My Draft'.
+  // Both h1 elements should contain the text 'MOCK MY DRAFT'.
   const h1Elements = await page.locator('h1').all();
   for (const h1Element of h1Elements) {
-    expect(await h1Element.innerText()).toContain('Mock My Draft');
+    expect(await h1Element.innerText()).toContain('MOCK MY DRAFT');
   }
 });
